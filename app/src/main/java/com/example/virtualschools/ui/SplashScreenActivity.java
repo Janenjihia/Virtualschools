1daf100b264fa191ea7877e1867dabba325c7e18
Charawey-X <charawey@gmail.com>
2022-07-05 00:01:42 +0300

    remove status bar

--- app/src/main/java/com/example/virtualschools/ui/SplashScreenActivity.java
+++ app/src/main/java/com/example/virtualschools/ui/SplashScreenActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.virtualschools.R;
 
@@ -13,6 +14,8 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
     }
 }
