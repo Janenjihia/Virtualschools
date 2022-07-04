95fae2e7ac99fb0a1b7b72b86542588f327a5aad
Janenjihia <jane.student@moringaschool.com>
2022-07-04 12:47:19 +0300

    Alter courses fragment

--- app/src/main/java/com/example/virtualschools/ui/CoursesFragment.java
+++ app/src/main/java/com/example/virtualschools/ui/CoursesFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.virtualschools.R;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CoursesFragment#newInstance} factory method to
