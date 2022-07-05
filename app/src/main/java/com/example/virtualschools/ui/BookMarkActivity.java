8319d31f84a760933471c384ea0feec5df7a5bb0
Charawey-X <charawey@gmail.com>
2022-07-05 14:12:43 +0300

    add bookmark activity

---
+++ app/src/main/java/com/example/virtualschools/ui/BookMarkActivity.java
@@ -0,0 +1,16 @@
+package com.example.virtualschools.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.virtualschools.R;
+
+public class BookMarkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bookmark_layout);
+    }
+}
