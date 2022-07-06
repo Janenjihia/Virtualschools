784c5525366d329ef7c6176f4d3e4e3699addcd1
Charawey-X <charawey@gmail.com>
2022-07-06 23:07:38 +0300

    add drawable

--- app/src/main/java/com/example/virtualschools/ui/ChatActivity.java
+++ app/src/main/java/com/example/virtualschools/ui/ChatActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.virtualschools.R;
+
 public class ChatActivity extends AppCompatActivity {
 
     @Override
