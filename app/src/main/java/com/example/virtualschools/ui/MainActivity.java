fbdab045f7f35122aa6029cf5a7b23c3a3b17c7f
Charawey-X <charawey@gmail.com>
2022-07-06 14:23:44 +0300

    add floating action buttons

--- app/src/main/java/com/example/virtualschools/ui/MainActivity.java
+++ app/src/main/java/com/example/virtualschools/ui/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.virtualschools.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-
 import android.annotation.SuppressLint;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.GridLayout;
 import android.widget.TextView;
 
