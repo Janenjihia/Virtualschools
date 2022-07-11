edfedf66833cdb25a10a2a3c84c51a2bc63027cc
Charawey-X <charawey@gmail.com>
2022-07-11 08:19:36 +0300

    connect users

--- app/src/main/java/com/example/virtualschools/ui/ProfileActivity.java
+++ app/src/main/java/com/example/virtualschools/ui/ProfileActivity.java
@@ -1,23 +1,39 @@
 package com.example.virtualschools.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.virtualschools.R;
+import com.example.virtualschools.models.StudentDetails;
+import com.example.virtualschools.network.Api;
+import com.example.virtualschools.network.Client;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.name) TextView studentName;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.role) TextView role;
 
     @SuppressLint("NonConstantResourceId")
     @BindView(R.id.back) ImageView back;
@@ -78,6 +94,31 @@ public class ProfileActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        //get profile details
+        Api client = Client.getClient();
+        //retrieve token
+        SharedPreferences preferences = getSharedPreferences("STUDENT_TOKEN", Context.MODE_PRIVATE);
+        String retrivedToken  = preferences.getString("TOKEN",null);
+
+        Call<StudentDetails> call = client.getStudentDetails("Bearer "+retrivedToken);
+        call.enqueue(new Callback<StudentDetails>() {
+            @Override
+            public void onResponse(@NonNull Call<StudentDetails> call, @NonNull Response<StudentDetails> response) {
+                if(response.isSuccessful()){
+                    assert response.body() != null;
+                    studentName.setText(response.body().getName());
+                    role.setText(response.body().getRole());
+                } else {
+                    Toast.makeText(ProfileActivity.this, "Bad Request", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<StudentDetails> call, @NonNull Throwable t) {
+                Toast.makeText(ProfileActivity.this, "Network Failure", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
